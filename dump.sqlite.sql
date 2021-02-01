25d2bfeea13b8ab91f5aae521158c6cdd9efae54
thammatan premwicha <53300265+thammatan@users.noreply.github.com>
2021-02-01 18:29:10 +0700

    Add files via upload
    create Database

---
+++ dump.sqlite.sql
@@ -0,0 +1,25 @@
+-- TABLE
+CREATE TABLE Cars (
+    ID int NOT NULL,
+    Band VARCHAR(255) NOT NULL,
+    PRIMARY KEY (ID)
+);
+CREATE TABLE Persons (
+    ID int NOT NULL,
+    Name varchar(255) NOT NULL,
+    PRIMARY KEY (ID)
+);
+CREATE TABLE PersonsJoinCars (
+    PersonsID int NOT NULL,
+    CarsID int NOT NULL,
+    PRIMARY KEY (PersonsID,CarsID),
+    FOREIGN KEY (PersonsID) REFERENCES Persons(ID),
+   FOREIGN KEY (CarsID) REFERENCES Cars(ID)
+);
+ 
+-- INDEX
+ 
+-- TRIGGER
+ 
+-- VIEW
+ 
